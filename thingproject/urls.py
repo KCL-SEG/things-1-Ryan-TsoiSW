944c2ac83e050f077766ca0f82aff9eb34717621
Ryan Tsoi <sanwingtsoi@gmail.com>
2022-12-08 07:05:39 +0000

    2.1

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from things import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(' ', views.thing  , name = 'thing')
+    path('/', views.thing  , name = 'thing')
 ]
--- things/views.py
+++ things/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
 def thing(request):
-
- 
     return render(request, 'thing.html')
 
 # Create your views here.
